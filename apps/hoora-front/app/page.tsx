fbba981d370d25d11b412e79f033e48e62e525c3
Pier2208 <p.leroy.mtl@gmail.com>
2024-05-23 15:51:11 -0400

    feat:tailwinf and shadcn-ui

--- apps/hoora-front/app/page.tsx
+++ apps/hoora-front/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@lpdv/ui/shadcn';
+
 export default async function Index() {
   return (
-    <div className="bg-slate-600 h-screen w-full flex items-center justify-center ">
+    <div className="bg-slate-600 h-screen w-full flex flex-col items-center justify-center ">
       <h1 className="text-white text-6xl">Hello world</h1>
+      <Button className="mt-8">Click me</Button>
     </div>
   );
 }
